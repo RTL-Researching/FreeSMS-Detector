2cc6759ac35384f5846982259a8138ebed7713fd
BradlyRyan <41997634+BradlyRyan@users.noreply.github.com>
2018-08-11 15:29:08 -0700

    minor cleanup

--- freesms-detector/scraper.py
+++ freesms-detector/scraper.py
@@ -8,22 +8,23 @@ from runner import insert_phonenumber
 def scrape_site1():
     url = "https://receive-a-sms.com/"
     response = requests.get(url)
-    content = response.content
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table', id="maintable")
-    links = table.find_all('a',href=True)
+    links = table.find_all('a', href=True)
     for link in links:
         print(link.text)
         insert_phonenumber(link.text, url)
 
 
-def scraper_site2():
+def scrape_site2():
     url = "https://sms-online.co/receive-free-sms"
     response = requests.get(url)
-    content = response.content
     soup = BeautifulSoup(response.text, 'html.parser')
     numbers = soup.select('h4.number-boxes-item-number')
 
     for number in numbers:
         print(number.text)
         insert_phonenumber(number.text, url)
+
+
+scrape_site1()
