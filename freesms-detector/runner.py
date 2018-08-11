26867705891ce46ee5177bcf2aa2983390b95acc
BradlyRyan <41997634+BradlyRyan@users.noreply.github.com>
2018-08-11 15:42:55 -0700

    merging legacy files into the one new runner.py

--- freesms-detector/runner.py
+++ freesms-detector/runner.py
@@ -3,7 +3,9 @@ import sqlite3
 import hashlib
 import os
 import re
-import json
+import requests
+from bs4 import BeautifulSoup
+
 
 def get_clean_phonenumber(phonenumber):
     # Tries to clean up a phonenumber according to the E.164 format.
@@ -13,7 +15,7 @@ def get_clean_phonenumber(phonenumber):
     try:
         pn = phonenumbers.parse(phonenumber, None)
     except phonenumbers.phonenumberutil.NumberParseException:
-        #print("Not a valid phone number: ", phonenumber)
+        # print("Not a valid phone number: ", phonenumber)
         return None
 
     pn = phonenumbers.format_number(pn, phonenumbers.PhoneNumberFormat.E164)
@@ -125,3 +127,36 @@ def get_complete_phonebook():
 
     return items
 
+
+# The scraping functions #
+
+
+def scrape_all():
+    print("Scraping all sites...")
+
+    print("scraping site 1")
+    scrape_site1()
+
+    print("scraping site 2")
+    scrape_site2()
+
+
+def scrape_site1():
+    url = "https://receive-a-sms.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', id="maintable")
+    links = table.find_all('a', href=True)
+    for link in links:
+        print(link.text)
+        insert_phonenumber(link.text, url)
+
+
+def scrape_site2():
+    url = "https://sms-online.co/receive-free-sms"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    numbers = soup.select('h4.number-boxes-item-number')
+    for number in numbers:
+        print(number.text)
+        insert_phonenumber(number.text, url)
